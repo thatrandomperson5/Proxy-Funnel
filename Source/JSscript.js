d22641e1a13256ba2aef216ad2d5a3c677d85b99
thatrandomperson5 <94306853+thatrandomperson5@users.noreply.github.com>
2022-06-17 18:53:51 -0400

    Update JSscript.js

--- Source/JSscript.js
+++ Source/JSscript.js
@@ -2,3 +2,49 @@ const caller = document.getElementById("jsproxyfunnel");
 const cfigPath = caller.dataset.cfig;
 console.log(cfigPath);
 import configData from cfigPath;
+function objToStrMap(obj) {
+  let strMap = new Map();
+  for (let k of Object.keys(obj)) {
+    strMap.set(k, obj[k]);
+  }
+  return strMap;
+}
+
+const config = objToStrMap(JSON.parse(configData));
+const appendFront = config.get("Fappend")
+const append = config.get("append")
+const localQ = config.get("localize")
+
+const callback = function(mutationList, observer) {
+
+  for (var mutation of mutationList) {
+    if (mutation.type === 'childList') {
+      var added = mutation.addedNodes
+      for (var node of added) {
+        if (node.src != undefined) {
+          var newSRC = node.src
+          if (localQ == true) {
+            newSRC = newSRC.replace("https://", "s§@")
+            newSRC = newSRC.replace("http://", "h§@")
+            newSRC = newSRC.split("/")
+            newSRC = newSRC.slice(1, newSRC.length).join("/")
+            console.log(newSRC)
+            if (appendFront != "") {
+              newSRC = appendFront + newSRC + append
+            }
+          }
+          newSRC = newSRC + append
+          console.log(newSRC)
+          node.src = newSRC
+
+        }
+      }
+    }
+  }
+};
+const evnconfig = {
+  childList: true,
+  subtree: true
+};
+const observer = new MutationObserver(callback);
+observer.observe(document.body, evnconfig);
